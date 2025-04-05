2e5b1178fd31c8d3ef031a72fc2cce69b0f4b012
Omkar <omkarsalunkhe3597@gmail.com>
2025-04-05 10:51:46 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import toast from 'react-hot-toast'
+
+function App() {
+	toast.success("Hi")
+	return (
+		<div>
+			Lets Begin
+		</div>
+	)
+}
+
+export default App
