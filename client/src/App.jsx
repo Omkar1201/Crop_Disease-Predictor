c52b4f9d4f7ae2f47084f86e73224bf6770172ed
Omkar <omkarsalunkhe3597@gmail.com>
2025-04-05 15:53:41 +0530

    framer motion added for selectfile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import './App.css'
-import toast from 'react-hot-toast'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import SelectFile from './components/SelectFile'
@@ -9,7 +6,7 @@ import About from './pages/About'
 function App() {
 
 	return (
-		<div className='pt-[5rem] '>
+		<div className='pt-[5rem] bg-gradient-to-b from-green-50 to-white'>
 			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
